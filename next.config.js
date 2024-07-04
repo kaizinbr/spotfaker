8836dc7675ff7bfda2327f9634036437543c3b63
Kaizin <kaiolucas1812@gmail.com>
2024-07-04 01:12:05 -0300

    img ajuste

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         domains: [
             "public.blob.vercel-storage.com",
             "i.scdn.co",
